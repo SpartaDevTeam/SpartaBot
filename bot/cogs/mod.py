ea86946ef5e3f4631d534a885d64ecc8194c2537
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-02-27 13:11:01 +0400

    added command to set a mute role

--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -145,6 +145,22 @@ class Moderation(commands.Cog):
         await member.remove_roles(mute_role)
         await ctx.send(f"**{member}** can speak now")
 
+    @commands.command(name="setmuterole", aliases=["setmute", "smr"], help="Set a role to give to people when you mute them")
+    @commands.has_guild_permissions(manage_roles=True)
+    async def set_mute_role(self, ctx, role: discord.Role):
+        Data.check_guild_entry(ctx.guild)
+
+        Data.c.execute(
+            "UPDATE guilds SET mute_role = :mute_role_id WHERE id = :guild_id",
+            {
+                "mute_role_id": role.id,
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        await ctx.send(f"The mute role has been set to {role.mention}")
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
