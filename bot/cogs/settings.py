386050f6eb70c4191e3d5387fb37450c84b6033b
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-03-12 16:51:52 +0400

    added welcome and leave settings and updated gitignore

--- bot/cogs/settings.py
+++ bot/cogs/settings.py
@@ -25,6 +25,76 @@ class Settings(commands.Cog):
 
         await ctx.send(f"The mute role has been set to {role.mention}")
 
+    @commands.command(name="setwelcomemessage", aliases=["wmsg"], help="Change the welcome message of the current server")
+    @commands.has_guild_permissions(administrator=True)
+    async def set_welcome_message(self, ctx, *, message: str = None):
+        Data.check_guild_entry(ctx.guild)
+
+        Data.c.execute(
+            "UPDATE guilds SET welcome_message = :new_message WHERE id = :guild_id",
+            {
+                "new_message": message,
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        if message:
+            await ctx.send(f"This server's welcome message has been set to:\n{message}")
+        else:
+            await ctx.send("This server's welcome message has been reset to default")
+
+    @commands.command(name="setleavemessage", aliases=["lmsg"], help="Change the leave message of the current server")
+    @commands.has_guild_permissions(administrator=True)
+    async def set_leave_message(self, ctx, *, message: str = None):
+        Data.check_guild_entry(ctx.guild)
+
+        Data.c.execute(
+            "UPDATE guilds SET leave_message = :new_message WHERE id = :guild_id",
+            {
+                "new_message": message,
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        if message:
+            await ctx.send(f"This server's leave message has been set to:\n{message}")
+        else:
+            await ctx.send("This server's leave message has been reset to default")
+
+    @commands.command(name="setwelcomechannel", aliases=["wchannel"], help="Change the channel where welcome messages are sent")
+    @commands.has_guild_permissions(administrator=True)
+    async def set_welcome_channel(self, ctx, *, channel: discord.TextChannel):
+        Data.check_guild_entry(ctx.guild)
+
+        Data.c.execute(
+            "UPDATE guilds SET welcome_channel = :channel_id WHERE id = :guild_id",
+            {
+                "channel_id": channel.id,
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        await ctx.send(f"The server's welcome channel has been set to {channel.mention}")
+
+    @commands.command(name="setleavechannel", aliases=["lchannel"], help="Change the channel where leave messages are sent")
+    @commands.has_guild_permissions(administrator=True)
+    async def set_leave_channel(self, ctx, *, channel: discord.TextChannel):
+        Data.check_guild_entry(ctx.guild)
+
+        Data.c.execute(
+            "UPDATE guilds SET leave_channel = :channel_id WHERE id = :guild_id",
+            {
+                "channel_id": channel.id,
+                "guild_id": ctx.guild.id
+            }
+        )
+        Data.conn.commit()
+
+        await ctx.send(f"The server's leave channel has been set to {channel.mention}")
+
 
 def setup(bot):
     bot.add_cog(Settings(bot))
