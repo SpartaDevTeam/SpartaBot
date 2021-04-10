a914c65d91c3626fe951f9a1fe04ca4e00554e2c
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-04-10 15:36:56 +0400

    new ipc route

--- bot/ipc_routes.py
+++ bot/ipc_routes.py
@@ -1,6 +1,15 @@
 from bot import bot
+from bot.data import Data
 
 
-@bot.ipc.route("get_guild_ids")
+@bot.ipc.route()
 async def get_guild_ids(data):
     return [guild.id for guild in bot.guilds]
+
+
+@bot.ipc.route()
+async def get_guild_info(data):
+    guild = await bot.fetch_guild(data.guild_id)
+
+    guild_info = {"name": guild.name, "icon_url": str(guild.icon_url)}
+    return guild_info
