f1371ed866663d66c7ed608a039bcf1630548547
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-04-13 12:52:00 +0400

    epic status

---
+++ bot/cogs/status.py
@@ -0,0 +1,46 @@
+import asyncio
+import discord
+from discord.ext import commands, tasks
+
+
+class Status(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+        self.theme_color = discord.Color.purple()
+        self.status_msgs = [
+            (discord.ActivityType.watching, "[guild_count] servers"),
+            (discord.ActivityType.playing, "Hypixel"),
+            (discord.ActivityType.listening, "Shawty's Like a Melody"),
+            (discord.ActivityType.streaming, "Minecraft Bedwars"),
+            (discord.ActivityType.playing, "Amogus"),
+            (discord.ActivityType.watching, "out for s!help"),
+        ]
+        self.status_index = 0
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.status_task.start()
+
+    def cog_unload(self):
+        self.status_task.cancel()
+
+    @tasks.loop(seconds=20)
+    async def status_task(self):
+        activity = self.status_msgs[self.status_index]
+        activ_type = activity[0]
+        activ_msg = activity[1]
+
+        if "[guild_count]" in activ_msg:
+            guild_count = len(self.bot.guilds)
+            activ_msg = activ_msg.replace("[guild_count]", str(guild_count))
+
+        activ = discord.Activity(type=activ_type, name=activ_msg)
+        await self.bot.change_presence(activity=activ)
+
+        self.status_index += 1
+        if self.status_index >= len(self.status_msgs):
+            self.status_index = 0
+
+
+def setup(bot):
+    bot.add_cog(Status(bot))
