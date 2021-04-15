39698b6d5fd93d3391513d0d0d4966a90241b3cf
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-04-15 22:58:26 +0400

    compeleted add auto response command

--- bot/cogs/auto_resp.py
+++ bot/cogs/auto_resp.py
@@ -1,3 +1,5 @@
+import asyncio
+import json
 import discord
 from discord.ext import commands
 
@@ -13,16 +15,68 @@ class AutoResponse(commands.Cog):
     @commands.command(
         name="addautoresponse",
         aliases=["addauto"],
-        help="Add an auto response phrase",
+        help="Add an auto response phrase. Example: addautoresponse this is the activation, this is the response",
     )
+    @commands.has_guild_permissions(administrator=True)
     async def add_auto_response_phrase(
-        self, ctx: commands.Context, options: str
+        self, ctx: commands.Context, *, options: str
     ):
         options_split = options.split(",", maxsplit=1)
-        activation_phrase = options_split[0].strip()
+
+        if len(options_split) < 2:
+            await ctx.send("Please provide all the fields.")
+            return
+
+        activation = options_split[0].strip()
         response = options_split[1].strip()
 
-        # TODO: add auto response
+        Data.c.execute(
+            "SELECT auto_responses FROM guilds WHERE id = :guild_id",
+            {"guild_id": ctx.guild.id},
+        )
+
+        current_auto_resps = json.loads(Data.c.fetchone()[0])
+
+        if activation in current_auto_resps:
+
+            def check_msg(message: discord.Message):
+                return (
+                    message.author == ctx.author
+                    and message.channel == ctx.channel
+                )
+
+            await ctx.send(
+                "An auto response with this activation already exists and will be overwritten by the new one. Do you want to continue? (Yes to continue, anything else to abort)"
+            )
+            try:
+                confirmation: discord.Message = await self.bot.wait_for(
+                    "message", check=check_msg, timeout=30
+                )
+
+                if confirmation.content.lower() == "yes":
+                    await ctx.send("Overwriting existing auto response!")
+                else:
+                    await ctx.send("Aborting!")
+                    return
+
+            except asyncio.TimeoutError:
+                await ctx.send("No response received, aborting!")
+                return
+
+        current_auto_resps[activation] = response
+
+        Data.c.execute(
+            "UPDATE guilds SET auto_responses = :new_responses WHERE id = :guild_id",
+            {
+                "new_responses": json.dumps(current_auto_resps),
+                "guild_id": ctx.guild.id,
+            },
+        )
+        Data.conn.commit()
+
+        await ctx.send(
+            f"New auto response added with\n\nActivation Phrase:```{activation}```\nResponse:```{response}```"
+        )
 
 
 def setup(bot):
