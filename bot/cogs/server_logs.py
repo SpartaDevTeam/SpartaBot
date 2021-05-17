70a405a6934374315c8c27c07ae96a7e70411e9a
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-05-17 09:57:56 +0400

    formatting

--- bot/cogs/server_logs.py
+++ bot/cogs/server_logs.py
@@ -2,7 +2,8 @@ import discord
 import datetime
 from discord.ext import commands
 
-class server_logs(commands.Cog):
+
+class ServerLogs(commands.Cog):
     def __init__(self, bot):
         self.bot: MyBot = bot
         self.description = "Shows when the bot joins or leaves a guild"
@@ -11,15 +12,33 @@ class server_logs(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         inv = await guild.text_channels[0].create_invite()
-        embed = discord.Embed(title='I Have Joined A New Guild!', description=f'[{guild.name}]({inv})', timestamp = datetime.datetime.now())
-        embed.add_field(name=f'This Guild Has {guild.member_count} Members!', value=f'Yay Another Server! We Are Now At {len(self.bot.guilds)} Guilds!')
-        await self.bot.get_guild(733224197827854377).get_channel(843726111360024586).send(embed=embed)
+        embed = discord.Embed(
+            title="I Have Joined A New Guild!",
+            description=f"[{guild.name}]({inv})",
+            timestamp=datetime.datetime.now(),
+        )
+        embed.add_field(
+            name=f"This Guild Has {guild.member_count} Members!",
+            value=f"Yay Another Server! We Are Now At {len(self.bot.guilds)} Guilds!",
+        )
+        await self.bot.get_guild(733224197827854377).get_channel(
+            843726111360024586
+        ).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_guild_leave(self, guild):
-        embed = discord.Embed(title='I Have lost A Guild!', description=f'{guild.name}', timestamp = datetime.datetime.now())
-        embed.add_field(name=f';-; We Are Now At {len(self.bot.guilds)} Guilds!')
-        await self.bot.get_guild(733224197827854377).get_channel(843726111360024586).send(embed=embed)
+        embed = discord.Embed(
+            title="I Have Left A Guild!",
+            description=f"{guild.name}",
+            timestamp=datetime.datetime.now(),
+        )
+        embed.add_field(
+            name=f";-; We Are Now At {len(self.bot.guilds)} Guilds!"
+        )
+        await self.bot.get_guild(733224197827854377).get_channel(
+            843726111360024586
+        ).send(embed=embed)
+
 
 def setup(bot):
-    bot.add_cog(server_logs(bot))
+    bot.add_cog(ServerLogs(bot))
