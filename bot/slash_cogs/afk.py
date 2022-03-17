2507365f76437182874d172e6dc7178bc0ea6a7c
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-03-17 21:39:31 +0400

    using asyncio.gather to process afks

--- bot/slash_cogs/afk.py
+++ bot/slash_cogs/afk.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -9,17 +10,19 @@ class SlashAFK(commands.Cog):
         self.bot = bot
 
     async def process_afk(self, message: discord.Message):
-        Data.c.execute("SELECT * FROM afks")
-        afks = Data.c.fetchall()
-
-        for afk_entry in afks:
+        async def run_afk(afk_entry):
             user = await self.bot.fetch_user(int(afk_entry[0]))
             if user in message.mentions:
                 afk_reason = afk_entry[1]
                 await message.channel.send(
-                    f"{message.author.mention}, {user} is currently AFK because:\n*{afk_reason}*",
+                    f"{user} is currently AFK because:\n*{afk_reason}*",
                     allowed_mentions=discord.AllowedMentions.none(),
                 )
+
+        Data.c.execute("SELECT * FROM afks")
+        afks = Data.c.fetchall()
+        afk_tasks = [run_afk(afk) for afk in afks]
+        await asyncio.gather(*afk_tasks)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
