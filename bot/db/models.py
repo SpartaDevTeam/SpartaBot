256e418d729daa2bfa2ea2014fd5a2ac8e1b5112
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-04-05 12:17:39 +0400

    added ORM models

--- bot/db/models.py
+++ bot/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, JSON, String, BigInteger, Integer
+from sqlalchemy import Column, JSON, String, BigInteger, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -28,3 +28,39 @@ class AFK(Base):
 
     user_id = Column(BigInteger, primary_key=True)
     message = Column(String, nullable=False)
+
+
+class ReactionRoles(Base):
+    __tablename__ = "reaction_roles"
+
+    id = Column(String, primary_key=True)
+    guild_id = Column(BigInteger, nullable=False)
+    channel_id = Column(BigInteger, nullable=False)
+    message_id = Column(BigInteger, nullable=False)
+    emoji = Column(String, nullable=False)
+    role_id = Column(BigInteger, nullable=False)
+
+
+class Reminders(Base):
+    __tablename__ = "reminders"
+
+    id = Column(String, primary_key=True)
+    user_id = Column(BigInteger, nullable=False)
+    message = Column(String, nullable=False)
+    start = Column(DateTime, nullable=False)
+    due = Column(DateTime, nullable=False)
+
+
+class Webhooks(Base):
+    __tablename__ = "webhooks"
+
+    channel_id = Column(BigInteger, primary_key=True)
+    webhook_url = Column(String, nullable=False)
+
+
+class AutoResponses(Base):
+    __tablename__ = "auto_responses"
+
+    id = Column(String, primary_key=True)
+    activation = Column(String, nullable=False)
+    response = Column(String, nullable=False)
